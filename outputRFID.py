20753f981115af05e374913d68d831b145f78d64
Ole Brodersen <brodersen.ole@gmail.com>
2018-09-26 06:10:52 +0200

    CHG: minor to rfid output

--- outputRFID.py
+++ outputRFID.py
@@ -15,6 +15,8 @@ while (True):
         rfid_input = str(raw_input('rfid input:')) # python2: raw_input; python3: input
         if(rfid_input == 'x'):
             break
+        else:
+            print("input was: " + rfid_input)
 
     except KeyboardInterrupt: #Strg-C wird gedrückt
         print("Exception raised.")
