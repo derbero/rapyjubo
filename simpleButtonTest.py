c4831fd03b7ed0d6de0932bff9af8e321cb92d13
Ole Brodersen <brodersen.ole@gmail.com>
2018-09-27 22:26:56 +0200

    chg

--- simpleButtonTest.py
+++ simpleButtonTest.py
@@ -3,10 +3,10 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(11)
+    input_state = GPIO.input(17)
     if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
