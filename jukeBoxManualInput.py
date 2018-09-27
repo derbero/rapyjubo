40d0e95b804bba4f53e2be7af0bdd533167aaa39
Ole Brodersen <brodersen.ole@gmail.com>
2018-09-27 22:07:44 +0200

    CHG

--- jukeBoxManualInput.py
+++ jukeBoxManualInput.py
@@ -398,7 +398,7 @@ mpdping_t.start() # Start it!
 
 while (True):
     try:
-        sleep(0.1)
+        sleep(1)
 #        if GPIO.event_detected(IN_PIN_TOGGLE_PLAY_PAUSE):
 #            mpd_client.PlayPauseToggle();
 #            print('IN_PIN_TOGGLE_PLAY_PAUSE button pressed')
