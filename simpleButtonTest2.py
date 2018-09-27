6b52d568164a37832798f3b8fd57164b53536fa2
Ole Brodersen <brodersen.ole@gmail.com>
2018-09-27 22:43:21 +0200

    add

---
+++ simpleButtonTest2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import types
+import sys
+from socket import error as SocketError
+from mpd import (MPDClient, CommandError)
+import mpd
+import threading
+# import mpc  # ?
+from time import sleep, time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+IN_PIN_TOGGLE_PLAY_PAUSE = 11
+
+# To configure a channel as an input:
+# GPIO.setup(channel, GPIO.IN)
+# [IN_PIN_VOLUME_UP, IN_PIN_VOLUME_DOWN, IN_PIN_TRACK_NEXT, IN_PIN_TRACK_PREVIOUS, IN_PIN_TOGGLE_PLAY_PAUSE]
+GPIO.setup(IN_PIN_VOLUME_UP, GPIO.IN)
+GPIO.setup(IN_PIN_VOLUME_DOWN, GPIO.IN)
+GPIO.setup(IN_PIN_TRACK_NEXT, GPIO.IN)
+GPIO.setup(IN_PIN_TRACK_PREVIOUS, GPIO.IN)
+GPIO.setup(IN_PIN_TOGGLE_PLAY_PAUSE, GPIO.IN)
+
+
+GPIO.add_event_detect(IN_PIN_TOGGLE_PLAY_PAUSE, GPIO.RISING, callback=mpdPlayPauseToggle, bouncetime=200)
+
+
+while (True):
+    try:
+        sleep(1)
+    except KeyboardInterrupt: #Strg-C wird gedrückt
+        print("Exception raised.")
+        print()
+        print("GPIO.cleanup()...")
+        GPIO.cleanup()
+        print("GPIO.cleanup()... done")
