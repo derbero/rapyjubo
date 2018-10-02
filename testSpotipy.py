6d4341fbf28e8aacd4fca3efa43224d646989b0d
derbero <43387026+derbero@users.noreply.github.com>
2018-10-02 15:35:35 +0200

    Update testSpotipy.py

--- testSpotipy.py
+++ testSpotipy.py
@@ -1,6 +1,4 @@
 import spotipy
-sp = spotipy.Spotify()
-
-results = sp.search(q='weezer', limit=20)
-for i, t in enumerate(results['tracks']['items']):
-    print ' ', i, t['name']
+spotify = spotipy.Spotify()
+results = spotify.search(q='artist:' + name, type='artist')
+print results
