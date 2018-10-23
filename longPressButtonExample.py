eeeea8e333260c97ac21a2b1e2ef3df4d4f27570
Ole Brodersen <brodersen.ole@gmail.com>
2018-10-23 06:00:52 +0200

    chg

--- longPressButtonExample.py
+++ longPressButtonExample.py
@@ -32,7 +32,8 @@ pressTick = pi.get_current_tick()  # initializing var
 pi.set_mode(gpio, pigpio.INPUT)
 pi.set_pull_up_down(gpio, pigpio.PUD_UP)  # this depends on how the switch is connected
 pi.set_glitch_filter(gpio, 300)
-cb = pi.callback(gpio, pigpio.RISING_EDGE, intCallback)
+#cb = pi.callback(gpio, pigpio.RISING_EDGE, intCallback)
+cb = pi.callback(gpio, pigpio.FALLING_EDGE, intCallback)
 
 
 
