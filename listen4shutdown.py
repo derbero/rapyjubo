bf4bfb6a799f0c5f9181af1bcad408b9b3c35c46
Ole Brodersen <brodersen.ole@gmail.com>
2018-10-23 05:42:36 +0200

    chg

--- listen4shutdown.py
+++ listen4shutdown.py
@@ -3,7 +3,7 @@
 # shutdown / power on Raspberry Pi with pushbutton
 
 # pushbutton: NO connected to GPIO 3, normally open and shuts at button press
-# Button press starts raspberry if off (connecting GPIO 3 to GND) and does nothing if raspberry is already on 
+# Button press starts raspberry if off (connecting GPIO 3 to GND) and does nothing if raspberry is already on
 
 import RPi.GPIO as GPIO
 from subprocess import call
@@ -18,15 +18,19 @@ shutdownPin = 4
 # power button has a LED that is connected to GND and GPIO 23
 # goal is to let it blink for some time when we are shutting down
 ledPin = 23
-ledState = False
+ledState = True
 
 # button debounce time in seconds
-#debounceSeconds = 0.01
-debounceSeconds = 1
 deltaToShutdown = 1
+resetFirstButtonPressTime = 2
+
+buttonPressedFirst = 0
+buttonPressedSecond = 0
+buttonPressedDelta = 0
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(shutdownPin, GPIO.IN)
 #GPIO.setup(shutdownPin, GPIO.IN)
 #GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(ledPin, GPIO.OUT, initial=0)
@@ -36,41 +40,43 @@ elapsed = 0
 
 def buttonStateChanged(pin):
  ledState = False
- print("button pressed")
- global buttonPressedTime
- global elapsed
+ #print("button pressed")
+ global buttonPressedFirst
+ global buttonPressedSecond
+ global buttonPressedDelta
 
- if not (GPIO.input(pin)):
-         # button is down
-         print("not GPIO.input(pin)")
-         # if buttonPressedTime is None:
-         elapsed = 0
-         buttonPressedTime = datetime.now()
+# if not (GPIO.input(pin)):
+ if GPIO.input(shutdownPin) == 1:
+         if (buttonPressedFirst == 0) or (time.time() - buttonPressedFirst > resetFirstButtonPressTime):
+             print("FIRST button press")
+             buttonPressedFirst = time.time()
+         else:
+            print("SECOND button press")
+            buttonPressedSecond = time.time()
+            buttonPressedDelta = buttonPressedSecond - buttonPressedFirst
+            buttonPressedFirst = 0
+            if buttonPressedDelta <= deltaToShutdown:
+                print("BOTH button presses happenend within one second; delta: " + str(buttonPressedDelta))
+                # toggle / blink ledPin for a while
+                for i in range(1, 11):
+                    ledState = not ledState
+                    GPIO.output(ledPin, ledState)
+                    sleep(0.1)
+                # shutdown
+                call(['shutdown', '-h', 'now'], shell=False)
  else:
-         print("GPIO.input(pin):")
-         print(pin)
-         # button is up
-         #if buttonPressedTime is not None:
-         elapsed = (datetime.now() - buttonPressedTime).total_seconds()
-         print ("elapsed: " + str(elapsed))
-         buttonPressedTime = datetime.now()
-
-         if elapsed >= debounceSeconds:
-              # button pressed for a shorter time, also shutdown
-              # toggle / blink ledPin for a while
-              for i in range(1, 11):
-                  ledState = not ledState
-                  GPIO.output(ledPin, ledState)
-                  sleep(0.1)
-              # shutdown
-              call(['shutdown', '-h', 'now'], shell=False)
+         print("BUTTON released - not HANDLING THAT")
 
 # subscribe to button presses
-GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged, bouncetime=400)
+#GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged, bouncetime=200)
+#GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged, bouncetime=200)
+GPIO.add_event_detect(shutdownPin, GPIO.FALLING, callback=buttonStateChanged, bouncetime=200)
 #GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged)
 
-while True:
+try:
+    while True:
          # sleep to reduce unnecessary CPU usage
          time.sleep(5)
-
-GPIO.cleanup()
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  print("\nBye!")
---
+++ listen4shutdownFirstTry.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+# shutdown / power on Raspberry Pi with pushbutton
+
+# pushbutton: NO connected to GPIO 3, normally open and shuts at button press
+# Button press starts raspberry if off (connecting GPIO 3 to GND) and does nothing if raspberry is already on 
+
+import RPi.GPIO as GPIO
+from subprocess import call
+from datetime import datetime
+import time
+from time import sleep
+
+# pushbutton: NC connected to GPIO 4 (former entry: 27), normally closed, and opens at button press
+# we want a shutdown if button is pressed meaning  GPIO 4 (former entry: 27) opens
+shutdownPin = 4
+
+# power button has a LED that is connected to GND and GPIO 23
+# goal is to let it blink for some time when we are shutting down
+ledPin = 23
+ledState = False
+
+# button debounce time in seconds
+#debounceSeconds = 0.01
+debounceSeconds = 1
+deltaToShutdown = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(shutdownPin, GPIO.IN)
+#GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(ledPin, GPIO.OUT, initial=0)
+
+buttonPressedTime = datetime.now()
+elapsed = 0
+
+def buttonStateChanged(pin):
+ ledState = False
+ print("button pressed")
+ global buttonPressedTime
+ global elapsed
+
+ if not (GPIO.input(pin)):
+         # button is down
+         print("not GPIO.input(pin)")
+         # if buttonPressedTime is None:
+         elapsed = 0
+         buttonPressedTime = datetime.now()
+ else:
+         print("GPIO.input(pin):")
+         print(pin)
+         # button is up
+         #if buttonPressedTime is not None:
+         elapsed = (datetime.now() - buttonPressedTime).total_seconds()
+         print ("elapsed: " + str(elapsed))
+         buttonPressedTime = datetime.now()
+
+         if elapsed >= debounceSeconds:
+              # button pressed for a shorter time, also shutdown
+              # toggle / blink ledPin for a while
+              for i in range(1, 11):
+                  ledState = not ledState
+                  GPIO.output(ledPin, ledState)
+                  sleep(0.1)
+              # shutdown
+              call(['shutdown', '-h', 'now'], shell=False)
+
+# subscribe to button presses
+GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged, bouncetime=400)
+#GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged)
+
+while True:
+         # sleep to reduce unnecessary CPU usage
+         time.sleep(5)
+
+GPIO.cleanup()
