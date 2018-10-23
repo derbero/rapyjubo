84accd22ff7ceaa2c3189c1a277ce75ecbd5b365
Ole Brodersen <brodersen.ole@gmail.com>
2018-10-23 04:54:03 +0200

    chg

--- listen4shutdown2.py
+++ listen4shutdown2.py
@@ -63,7 +63,7 @@ def buttonStateChanged(pin):
               call(['shutdown', '-h', 'now'], shell=False)
 
 # subscribe to button presses
-GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged, bouncetime=220)
+GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged, bouncetime=240)
 #GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged)
 
 try:
