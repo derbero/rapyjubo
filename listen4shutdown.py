7b84b1ffe443852400123c7869f4f9007debcf89
Ole Brodersen <brodersen.ole@gmail.com>
2018-10-25 05:21:35 +0200

    chg

--- listen4shutdown.py
+++ listen4shutdown.py
@@ -22,7 +22,7 @@ shutdownPin = 4
 def setup_custom_logger(name):
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
-    handler = logging.FileHandler('/home/pi/rapyjubo/listen4shutdown.log', mode='w')
+    handler = logging.FileHandler('/home/pi/rapyjubo/listen4shutdown_run.log', mode='w')
     handler.setFormatter(formatter)
     screen_handler = logging.StreamHandler(stream=sys.stdout)
     screen_handler.setFormatter(formatter)
